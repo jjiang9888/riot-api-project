43d5264b0c86ef88078dae33ea388f01fd78565d
jjiang9888 <jjiang9888@gmail.com>
2022-12-22 15:40:09 -0500

    edit

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ import chalk from "chalk";
 
 mongoose.set("returnOriginal", false);
 
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/riotAPI'
+const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/riotAPI';
 
 mongoose.connect(url).catch((err)=> {
     console.log(`Error connection to MongoDB:${err.message}`);
